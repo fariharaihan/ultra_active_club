5e408fd0867514d4914b74f3ab705605035b2027
fariharaihan <liafariha5@gmail.com>
2022-09-29 07:51:47 +0600

    header add

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Header.css'
+
+const Header = () => {
+    return (
+        <nav>
+            <div className='header'>
+                <img src='' alt='' />
+                <h2 className='header-text'>Fitness Club</h2>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
