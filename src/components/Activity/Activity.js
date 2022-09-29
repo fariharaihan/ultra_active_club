8182c03e181a9f047a79543364e31aec206f59de
fariharaihan <liafariha5@gmail.com>
2022-09-29 12:28:50 +0600

    handleaddlist

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './Activity.css'
 
-const Activity = (props) => {
+const Activity = ({ activity, handleAddToList }) => {
     // console.log(props.activity)
-    const { image, name, age, time } = props.activity;
+    // const { activity, handleAddToList } = props
+    const { image, name, age, time } = activity;
+
     return (
         <div className='activity'>
             <img src={image} alt="" ></img>
             <div className='activity-info'>
                 <p className='activity-name'>{name}</p>
                 <p>For age: {age}</p>
-                <p>Time required: {time}</p>
+                <p>Time required: {time} m</p>
             </div>
+            <button onClick={() => handleAddToList(activity)} className='btn-list'>
+                <p>Add To List</p>
+            </button>
         </div>
     );
 };
--- src/components/Club/Club.js
+++ src/components/Club/Club.js
@@ -4,12 +4,20 @@ import './Club.css';
 
 const Club = () => {
     const [activities, setActivities] = useState([]);
+    const [details, setDetails] = useState([])
 
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
             .then(data => setActivities(data))
     }, [])
+
+    const handleAddToList = (activity) => {
+        console.log(activity)
+        const newDetails = [...details, activity];
+        setDetails(newDetails);
+
+    }
     return (
         <div className='club-container'>
             <div className="exercise-container">
@@ -17,11 +25,13 @@ const Club = () => {
                     activities.map(activity => <Activity
                         key={activity.id}
                         activity={activity}
+                        handleAddToList={handleAddToList}
                     ></Activity>)
                 }
             </div>
             <div className="exercise-details-container">
                 <h4>exercise details</h4>
+                <p>selected: {details.length}</p>
             </div>
         </div>
     );
